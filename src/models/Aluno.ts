4dfd070e1bd52f8a8e363ab2614934c3fe1a78f1
Alberto Duran <albertoduranfilho@gmail.com>
2026-03-02 15:50:01 -0300

    :sparkles: feat: Adicionando a Entidade Aluno.

---
+++ src/models/Aluno.ts
@@ -0,0 +1,10 @@
+// import type Categoria from "./Categoria";
+
+export default interface Postagem {
+  id: number;
+  nome: string;
+  telefone: string;
+  altura: number;
+  peso: number;
+  // categoria: Categoria | null;
+}
