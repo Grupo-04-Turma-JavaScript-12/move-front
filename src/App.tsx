2930e64e8c04ff7c4dbd099689a5224e2c0abd8a
Renato Sales <renatosalespaula@gmail.com>
2026-03-03 09:12:16 -0300

    codificação do componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import ListaAlunos from "./components/aluno/listaalunos/ListaAlunos";
+import ListaCategorias from "./components/categoria/listacategorias/ListaCategorias";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
-      <div>
-        {/* LEMBREM DE QUANDO SUBIR NO GIT DEIXAR DESSE MESMO JEITO PARA NÃO TERMOS CONFLITOS!*/}
-        <h1 className="text-9xl text-red-900">
-          LEMBREM DE QUANDO SUBIR NO GIT DEIXAR DESSE MESMO JEITO PARA NÃO
-          TERMOS CONFLITOS!
-        </h1>
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/alunos" element={<ListaAlunos />} />
+            <Route path="/categorias" element={<ListaCategorias />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
